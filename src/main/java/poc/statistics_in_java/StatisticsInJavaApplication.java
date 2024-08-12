09a23752ee8931a58bca5112a467b9fd89dfc654
Slaven Cvijetic <slaven.cvijetic@ruag.ch>
2024-08-12 11:19:50 +0200

    Extend t-test to account whether the computed mean is bigger than the population mean in a significant way (in R, this is the option alternative = "greater" in the t.test function)

--- src/main/java/poc/statistics_in_java/StatisticsInJavaApplication.java
+++ src/main/java/poc/statistics_in_java/StatisticsInJavaApplication.java
@@ -1,6 +1,7 @@
 package poc.statistics_in_java;
 
 import org.apache.commons.math3.distribution.NormalDistribution;
+import org.apache.commons.math3.distribution.TDistribution;
 import org.apache.commons.math3.stat.StatUtils;
 import org.apache.commons.math3.stat.inference.TTest;
 import org.apache.commons.math3.stat.inference.TestUtils;
@@ -13,6 +14,11 @@ import java.net.InetAddress;
 @SpringBootApplication
 public class StatisticsInJavaApplication {
 
+	/**
+	 * Keep in mind that the Java stats library does not have a direct way to test if the observed values are
+	 * greater or less than a given mean, which is easily possible in R
+	 * @param args, command line args if needed
+	 */
 	public static void main(String[] args) {
 		SpringApplication.run(StatisticsInJavaApplication.class, args);
 		System.out.println("\n***Starting Statistics test***");
@@ -27,6 +33,8 @@ public class StatisticsInJavaApplication {
 			double[] riggedLatencies = new double[runs*measurements];
 			double riggedMean = 15000000.0;
 			double riggedStandardDeviation = 10000.0;
+			double riggedModifier = 1.01;  // scales the sampled values by a little bit for sanity testing
+
 			NormalDistribution normalDistribution = new NormalDistribution(riggedMean, riggedStandardDeviation);
 
 			// outer loop which collects the measurements per run (resp. per experiment)
@@ -44,7 +52,7 @@ public class StatisticsInJavaApplication {
 
 					if (isReachable) {
 						latencies[measurement + measurements*run] = endTime - startTime;
-						riggedLatencies[measurement + measurements*run] = normalDistribution.sample();
+						riggedLatencies[measurement + measurements*run] = normalDistribution.sample()*riggedModifier;
 					}
 
 				}
@@ -53,21 +61,28 @@ public class StatisticsInJavaApplication {
 			// perform t-Test
 			double alpha = 0.01;
 			double populationMean = riggedMean;
+			double degreesOfFreedom = runs*measurements - 1; // df = N - #groups, #groups = 1 (one-sample t-test)
 			TTest tTest = new TTest();
+			TDistribution tDistribution = new TDistribution(degreesOfFreedom);
 
 			var isSignificant = tTest.tTest(populationMean, latencies, alpha*2);
+			// Calculate the critical value for the upper tail
+			double criticalValue = tDistribution.inverseCumulativeProbability(1 - (alpha/2));  // alpha/2 due to being one-sided
 
 			System.out.println("\n***Perform test with observed values***");
 			// false => sample mean is drawn from the same population as populationMean, which is good
 			// thus, the sample mean is not significantly different from populationMean under a given alpha
 			// (and confidence interval with confidence level = 1-alpha)
 			System.out.println("Result from t-Test: " + isSignificant);
-
 			System.out.println("Mean of observed values: " + StatUtils.mean(latencies));
 			System.out.println("SD of observed values: " + Math.sqrt(StatUtils.variance(latencies)));
 			System.out.println("is significant (H_0 is rejected): " + TestUtils.tTest(populationMean, latencies, alpha*2));  // *2 because it is one-sided
 			System.out.println("p-value: " + TestUtils.tTest(populationMean, latencies));
 			System.out.println("t-statistic: " + TestUtils.t(populationMean, latencies));
+			// calculate the critical values to better check if the observed values are above or below
+			System.out.println("Critical Value: " + criticalValue);
+			System.out.println("Comparing the means: computedMean/populationMean = " + StatUtils.mean(latencies)/populationMean);
+			System.out.println("t-Statistic > Critical Value: " + (TestUtils.t(populationMean, latencies) > criticalValue));
 
 
 			System.out.println("\n***Perform test with RIGGED values***");
@@ -75,6 +90,12 @@ public class StatisticsInJavaApplication {
 			System.out.println("is significant (H_0 is rejected): " + TestUtils.tTest(populationMean, riggedLatencies, alpha*2));  // *2 because it is one-sided
 			System.out.println("p-value: " + TestUtils.tTest(populationMean, riggedLatencies));  // *2 because it is one-sided
 			System.out.println("t-statistic: " + TestUtils.t(populationMean, riggedLatencies));
+			// calculate the critical values to better check if the observed values are above or below
+			System.out.println("Critical Value: " + criticalValue);
+			System.out.println("Comparing the means: computedMean/populationMean = " + StatUtils.mean(riggedLatencies)/populationMean);
+			System.out.println("t-Statistic > Critical Value: " + (TestUtils.t(populationMean, riggedLatencies) > criticalValue));
+
+
 
 		} catch (InterruptedException | IOException e) {
             throw new RuntimeException(e);
